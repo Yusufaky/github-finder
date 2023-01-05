67310d58f9c92edc7afab146827d598daa21193b
Yusufaky <marcosta71999@gmail.com>
2023-01-05 22:20:58 +0000

    50

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="text-2xl">This is a simple app to search Github users.</p>
+    </>
+  );
+}
+
+export default About;
