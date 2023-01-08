b462929cd875aeadfc0bbfec1c2fd9dbbbcd707a
Yusufaky <marcosta71999@gmail.com>
2023-01-08 21:58:12 +0000

    59

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,44 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GitHubReducer";
+const GithubContext = createContext();
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: false,
+  };
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading();
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {});
+    const { items } = await response.json();
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+  };
+
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+
+  // Set loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+export default GithubContext;
