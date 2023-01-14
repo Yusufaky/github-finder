c4b7e28f9c9f8fad42ce1ef08411b7b9c0e21c38
Yusufaky <marcosta71999@gmail.com>
2023-01-14 11:09:37 +0000

    65

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -35,7 +35,15 @@ export const GithubProvider = ({ children }) => {
   const getUserRepos = async (login) => {
     setLoading();
 
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos`, {});
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+    
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {}
+    );
 
     const data = await response.json();
 
